bc4b180d9948a2683171b963af8f9a242ee52a60
KonstantinovIKM723a <alex7wrt@gmail.com>
2025-03-28 18:24:46 +0200

    Update

--- lab_3-variant_9-2/Program.cs
+++ lab_3-variant_9-2/Program.cs
@@ -1,2 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Enter array length (from 2 to 32)");
+int arraySize = int.Parse(Console.ReadLine()!);
+
+if (arraySize < 2) {
+    arraySize = 2;
+}
+else if (arraySize > 32) {
+    arraySize = 32;
+}
+
+int[] array = new int[arraySize];
+Console.Write("Array numbers:");
+
+for (int i = 0; i < array.Length; i++) {
+    array[i] = new Random().Next(0, 100);
+    Console.Write(" " + array[i].ToString());
+}
+Console.WriteLine("");
+
+Console.WriteLine("Enter the Number");
+int N = int.Parse(Console.ReadLine()!);
+string pairs = "";
+
+for (int i = 0; i < array.Length - 1; i++) {
+    for (int j = i + 1; j < array.Length; j++) {
+    
+        if (array[i] + array[j] == N)
+        {
+            pairs += "(" + array[i].ToString() + ", " + array[j].ToString() + "); ";
+          
+        }
+
+    }
+}
+
+Console.WriteLine(pairs == "" ? "There are no pairs whose sum is equal to " + N.ToString() : "Pairs whose sum is equal to " + N.ToString() + ": " + pairs);
